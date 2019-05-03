c2904c22675a1c7e5122c8892190c25db0484f16
Sanmoo <sanmoo.cs@gmail.com>
2019-05-03 10:03:57 -0700

    Fix login screen layout

--- app/components/Login/Login.js
+++ app/components/Login/Login.js
@@ -8,14 +8,23 @@ const Container = styled.div`
   flex-direction: column;
   flex-grow: 1;
   justify-content: center;
-  align-items: center;
+  align-items: stretch;
   height: 100vh;
+  width: 100vw;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+`;
+
+const InnerContainer = styled.div`
   border: 1px solid rgba(6, 6, 6, 0.5);
   border-radius: 0.5rem;
-  margin-top: 6rem;
-  margin-bottom: 6rem;
-  margin-left: 2rem;
-  margin-right: 1rem;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default function Login({ onGetAuthToken }) {
@@ -31,14 +40,16 @@ export default function Login({ onGetAuthToken }) {
 
   return (
     <Container>
-      <p>Welcome to Simple Finance Manager</p>
-      <GoogleLogin
-        clientId={process.env.GOOGLE_CLIENT_ID}
-        buttonText="LOGIN WITH GOOGLE"
-        onSuccess={googleAuthCallback}
-        onFailure={googleAuthCallback}
-        autoLoad
-      />
+      <InnerContainer>
+        <p>Welcome to Simple Finance Manager</p>
+        <GoogleLogin
+          clientId={process.env.GOOGLE_CLIENT_ID}
+          buttonText="LOGIN WITH GOOGLE"
+          onSuccess={googleAuthCallback}
+          onFailure={googleAuthCallback}
+          autoLoad
+        />
+      </InnerContainer>
     </Container>
   );
 }
